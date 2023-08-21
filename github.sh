5444d3a7a4252818fd516c2a81fa8ca4bfa0951c
TthiagoS <toszgg.123@gmail.com>
2023-08-21 11:07:02 -0300

    branch com o código da aula do dia Mon, Aug 21, 2023 11:07:02 AM

--- github.sh
+++ github.sh
@@ -4,7 +4,7 @@
 #na variável repositório, prefira a url do repositório baseada em https para nao termos que configurar SSH
 export name='TthiagoS'
 export email="toszgg.123@gmail.com"
-export repositorio="https://github.com/TthiagoS/somarTSH.git";
+export repositorio="https://github.com/TthiagoS/somarTSH.git"
 
 #Daqui pra baixo mexa somente se for realmente necessário ou se souber o que está fazendo :P
 
